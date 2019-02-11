81c14fc5f6b3841c5c1115f57bc29a2440956a9a
Mark Pothecary <markpothers@hotmail.com>
2019-02-11 02:38:08 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+def my_collect(collection)
+  if block_given?
+    new_collection = []
+
+    x = 0
+    while x < collection.length
+      yield collection[x]
+      x += 1
+    end
 
 
+    new_collection
+  else
+    print "This block should not run!"
+  end
+end
