df9c25fd161bea673cbafb2b8644f6120cc7e8a6
Mark Pothecary <markpothers@hotmail.com>
2019-02-11 02:42:41 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+def my_collect(collection)
+  if block_given?
+    new_collection = []
+
+    x = 0
+    while x < collection.length
+      new_collection.push(yield collection[x])
+      x += 1
+    end
 
 
+    new_collection
+  else
+    print "This block should not run!"
+  end
+end
